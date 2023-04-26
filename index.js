5350bf4e335ecbac4aebf3a242eed60f9e91a367
Vaughn <vaughnchan8@gmail.com>
2023-04-26 00:22:32 -0400

    Add update_colors function to color specific indicies
    This change makes it so that the bubble sort function can modify the
    colors of the bars it is swapping, so as to better indicate which bars
    are being swapped.
    
    update_colors is used in bubble_sort, and is called before update_chart_data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const histogram_ref = document.getElementById("histogram");
-const arr_size = 11;
+const arr_size = 50;
 const num_range = 10
-const x = []
+var x = []
 var y = [];
 
 // Generate random array
@@ -38,26 +38,47 @@ histogram = new Chart(histogram_ref, {
 })
 
 /**
+ * Apply a color to all bars in the histogram, with some of them being
+ * different
+ *
+ * @param {chart}		Chart to update (will change dataset)
+ * @param {indicies_to_outline}	Array of indexes in the dataset to change
+ */
+function update_colors(chart, indicies_to_outline) {
+    bgColor = [];
+    
+    // First, set everything to one color...
+    for (let i = 0; i < arr_size; i++) {
+	bgColor[i] = 'rgba(75, 0, 130)'
+    }
+
+    // ...Then, color the specificly chosen indicies another color
+    indicies_to_outline.forEach((index) => {
+	bgColor[index] = 'rgba(255, 192, 203)'
+    });
+    
+    chart.data.datasets[0].backgroundColor = bgColor;
+}
+
+/**
  * Update the chart with the new values, with optional timer.
  *
  * @param {chart}		The chart to edit (WILL MODIFY THE DATASET)
  */
 function update_chart_data(chart) {
     chart.data.datasets[0].data = y;
-    chart.update();
+    chart.update('none');
     return new Promise(resolve => {
 	setTimeout(() => {
 	    resolve('resolved');
-	}, 200);
+	}, 1);
     });
 };
 
-
-// The Bubble Sort Algorithm
 /**
  * Bubble sort algorithm, with visualization update.
  *
- * @return			The array to sort, directly referenced
+ * @param {arr}			The array to sort, directly referenced
  */
 async function bubble_sort(arr) {
 
@@ -67,7 +88,12 @@ async function bubble_sort(arr) {
     while (!is_sorted) {
 	is_sorted = true;
 	for (let i = 0; i < arr.length - do_not_count; i++) {
+
+	    // Updating colors and graph
+	    update_colors(histogram, [i, i+1]);
 	    await update_chart_data(histogram);
+
+	    
 	    if (arr[i] > arr[i + 1]) {
 		is_sorted = false;
 		
@@ -75,10 +101,18 @@ async function bubble_sort(arr) {
 		arr[i] = arr[i] - arr[i + 1];
 		arr[i + 1] = arr[i] + arr[i + 1];
 		arr[i] = arr[i + 1] - arr[i];
+
+		// Updating colors and graph...again
+		update_colors(histogram, [i, i+1]);
+		await update_chart_data(histogram);
 	    }
 	}
 	do_not_count++;
     }
+
+    // Updating colors and graph one final time
+    update_colors(histogram, x);
+    await update_chart_data(histogram);
 };
 
 bubble_sort(y)
